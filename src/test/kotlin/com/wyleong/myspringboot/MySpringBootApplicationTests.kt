19f930bd794b16660066a9dce6be49a164f2491c
Leong Wai Yin <668915+laxa88@users.noreply.github.com>
2021-11-22 10:04:24 +0900

    Initial commit

---
+++ src/test/kotlin/com/wyleong/myspringboot/MySpringBootApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.wyleong.myspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MySpringBootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
