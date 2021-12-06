e42aa21fa04b8c9d89da4c3eb51d32a27a59fa09
Leong Wai Yin <668915+laxa88@users.noreply.github.com>
2021-12-06 20:23:56 +0900

    update test to use single instance for whole class

--- application/src/test/kotlin/com/example/multimodule/application/service/StudentServiceTest.kt
+++ application/src/test/kotlin/com/example/multimodule/application/service/StudentServiceTest.kt
@@ -2,33 +2,43 @@ package com.example.multimodule.application.service
 
 import com.example.multimodule.application.model.Student
 import com.example.multimodule.application.repository.StudentRepository
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 /**
  * Note: Prefer @SpringBootTest to @DataR2dbcTest, because we want to
  * also include the Flyway migrations so that the DB structure for tests
  * always matches the application.
+ *
+ * Note: @BeforeAll by default is static, but Kotlin doesn't allow static
+ * methods. TestInstance.Lifecycle.PER_CLASS enables non-static BeforeAll
+ * methods to be used.
  */
 @SpringBootTest
 @Testcontainers
 @ActiveProfiles("test") // reads from application-{profile_name}.yml
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class StudentServiceTest {
 
     @Autowired
     lateinit var studentRepository: StudentRepository
 
     // Note: @Container will recreate a new instance between each tests... Which is slow.
-    @Container
+    // @Container
     val container = PostgreSQLContainer<Nothing>("postgres").apply {
+        //
+        // withReuse(true)
+
         /**
          * These are not necessary; Testcontainers will automagically connect via `application-test.yml`.
          * Below config can be used for testing against actual DB, but prefer Testcontainers
@@ -39,6 +49,11 @@ internal class StudentServiceTest {
         // withExposedPorts(1111)
         // withUsername("postgres")
         // withPassword("password")
+    }
+
+    @BeforeAll
+    fun beforeAll() {
+        container.start()
     }
 
     @Test
@@ -54,6 +69,24 @@ internal class StudentServiceTest {
 
             // When
             studentRepository.save(newStudent)
+
+            // Then
+            val updated = studentRepository.findById(newStudent.id!!)
+            Assertions.assertThat(updated).isEqualTo(newStudent)
+        }
+    }
+
+    @Test
+    fun `should save 2`() {
+        runBlocking {
+            // Given
+            val student = Student(id = 2, name = "dName2", courseName = "dCourse2")
+
+            // When
+            val newStudent = studentRepository.save(student)
+
+            val foo = studentRepository.findAll().toList()
+            println("### $foo")
 
             // Then
             val updated = studentRepository.findById(newStudent.id!!)
